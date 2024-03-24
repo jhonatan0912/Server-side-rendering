08c6cebd0eaff4c486c696a8f5d9cfa27ea1c34a
Jhonatan <jhoonatan_0912@hotmail.com>
2024-03-23 20:56:23 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+interface Todo {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,6 +16,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ssr';
+export class AppComponent implements OnInit {
+
+  private http = inject(HttpClient);
+  todos = signal<Todo[]>([]);
+
+  ngOnInit(): void {
+    this.http.get('https://jsonplaceholder.typicode.com/todos')
+      .subscribe((todos: any) => {
+        this.todos.set(todos);
+      });
+  }
 }
